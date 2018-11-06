479f460d6ba1d456dd42f9b0b135645896913a1c
0918nobita <nobita.0918@gmail.com>
2018-11-06 14:12:00 +0900

    :construction: CountIncrement, CountDecrement アクションを追加した

--- src/app/counter/state/counter.actions.ts
+++ src/app/counter/state/counter.actions.ts
@@ -1,11 +1,20 @@
 import { Action } from '@ngrx/store';
 
 export enum CounterActionTypes {
-  LoadCounters = '[Counter] Load Counters'
+  // Action ごとに型を定義する
+  CountIncrement = '[Counter] Increment Count',
+  CountDecrement = '[Counter] Decrement Count'
 }
 
-export class LoadCounters implements Action {
-  readonly type = CounterActionTypes.LoadCounters;
+// Action ごとに @ngrx/store の Action インタフェースを実装したクラスを定義する
+// 複雑な処理をする場合はコンストラクタで引数を取る
+export class CountIncrement implements Action {
+  readonly type = CounterActionTypes.CountIncrement;
 }
 
-export type CounterActions = LoadCounters;
+export class CountDecrement implements Action {
+  readonly type = CounterActionTypes.CountDecrement;
+}
+
+// 上記で定義した Action 実装クラスを集約した型を定義する ( Reducer で使用する )
+export type CounterActions = CountIncrement | CountDecrement;
