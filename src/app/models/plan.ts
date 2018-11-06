26d377ec9dae1e4191fb12c5f030b4f03e9aae3d
0918nobita <nobita.0918@gmail.com>
2018-11-07 01:49:14 +0900

    :pencil2: Plan インタフェースの book プロパティの型を ResolvedBook | RegisteredBook に変更した

--- src/app/models/plan.ts
+++ src/app/models/plan.ts
@@ -1,9 +1,10 @@
+import { ResolvedBook } from './resolved-book';
 import { RegisteredBook } from './registered-book';
 
 export interface Plan {
   title: string;
   desc: string;
-  book: RegisteredBook;
+  book: ResolvedBook | RegisteredBook;
   progress: number;
   uid: string;
   created: firebase.firestore.Timestamp;
