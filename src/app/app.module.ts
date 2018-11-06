589ad7d17d2797fc2e2936ea41032e2616fa8eb1
0918nobita <nobita.0918@gmail.com>
2018-11-06 14:50:40 +0900

    :rotating_light: Remove linter warnings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { CounterModule } from "./counter/counter.module";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [AppComponent],
