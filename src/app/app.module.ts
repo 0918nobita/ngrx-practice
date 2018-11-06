192d9ff6b086ed9049fa14ca66eee0c342801ad6
0918nobita <nobita.0918@gmail.com>
2018-11-06 16:53:39 +0900

    :wrench: @ngrx/store-devtools の設定を app.module.ts に追加した

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CounterModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CounterModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
